dc72bc3d57dd2ae6b6c49887e8b7794aa80a222d
ochiengowino <benjaminochieng99@gmail.com>
2023-11-24 13:02:29 +0300

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using HotelBookingAPI.Data;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Db Connection for API
+// builder.Services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("BookingDb"));
+
+
+// Service for Db Connection
+builder.Services.AddDbContext<HotelBookingAPI.Data.ApiContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+
+
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
